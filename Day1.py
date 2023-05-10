27996fd4723e49d133950904e4a3f3612b64a299
Ryan Schlimme <98181475+ryanschlimme@users.noreply.github.com>
2023-05-10 17:24:05 -0500

    Title Updates
    Added creation date, name, and description

--- Day1.py
+++ Day1.py
@@ -1,3 +1,7 @@
+# 10 May 2023
+# Ryan Schlimme
+# This test script takes a downloaded tdms file from LABVIEW of random laser noise and analyzes its frequency spectrum
+
 f_name = r"C:\Users\Ryan Schlimme\OneDrive\Desktop\Research\Data\iter_0.tdms"
 
 import sys
@@ -8,10 +12,8 @@ C = CollectionTDMS(f_name) 		# class to apply various functions to time series d
 
 
 C.set_collection("Y") # select channel Y
-C.apply("bin_average", Npts = 100, inplace = True) # reduces data size by factor of 100 by 
-	# averaging over Npts
-C.average("PSD", taumax = 20e-3) #averages and sets equal to C.freq and C.psd and C.Navg_psd 					#(how many averages), taumax splits time domain to generate
-					# more averages
+C.apply("bin_average", Npts = 100, inplace = True) # reduces data size by factor of 100 by averaging over Npts
+C.average("PSD", taumax = 20e-3) #averages and sets equal to C.freq and C.psd and C.Navg_psd (how many averages), taumax splits time domain to generate more averages
 
 print(C.Navg_psd)
 plt.loglog(C.freq, C.psd)
