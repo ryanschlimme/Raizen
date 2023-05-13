0d37f93a669be677fbc6639c4fe5dd19fced106c
Ryan Schlimme <98181475+ryanschlimme@users.noreply.github.com>
2023-05-13 11:29:09 -0500

    Update t-axis, Added Individual Pulse Plots
    Updated t-axis to microseconds using set ticks workaround (need to install LaTeX natively to support taxis = "u" argument in plot statement). Coded individual pulse plots. Want to work to create faceted plot to compare pulses. {Issues: remove first two subplots which are blank, share axis labels between plots, manage pulse numbering}

--- 05-12-2023, Time Domain.py
+++ 05-12-2023, Time Domain.py
@@ -7,7 +7,7 @@
 f_name = r"C:\Users\Ryan Schlimme\OneDrive\Desktop\Research\Data\ablation pulse.tdms" # create a variable pointing to file (change Ryan Schlimme to ryans)
 import sys													# allows access to path.append
 sys.path.append(r"C:\Users\Ryan Schlimme\OneDrive\Desktop\Research\brownian\src") 	# append path to brownian src folder (change Ryan Schlimme to ryans)
-from time_series import CollectionTDMS 								# pull function from time_series module
+from time_series import CollectionTDMS								# pull function from time_series module
 L = CollectionTDMS(f_name)
 L.set_collection("X")
 M = CollectionTDMS(f_name)
@@ -26,14 +26,38 @@ Npts = L.r / (2*500000)
 L.apply("bin_average", Npts = Npts, inplace = True)
 M.apply("bin_average", Npts = Npts, inplace = True)
 
+
+##### Individual Pulse Plots #####
+I = list(range(2,11))
+for i in I:
+	fig, ax = plt.subplots(1)
+	Li = L.collection[i]
+	Mi = M.collection[i]
+	Li.plot(tmin=2.8e-4, tmax = 3e-4, ax = ax, c = "r")
+	Mi.plot(tmin=2.8e-4, tmax = 3e-4, ax = ax, c = "b")
+	plt.suptitle("Acoustic Signal as Recorded by Microphone and Laser Deflection")
+	pulse_num = "Pulse #" + str(i)
+	plt.title = pulse_num
+	x_ticks = [0.0002800, 0.0002825, 0.0002850, 0.0002875, 0.0002900, 0.0002925, 0.0002950, 0.0002975, 0.0003000]
+	x_labels = [280.0, 282.5, 285.0, 287.5, 290.0, 292.5, 295.0, 297.5, 300.0]
+	plt.xticks(ticks = x_ticks, labels = x_labels)
+	plt.xlabel("Time (us)")
+	plt.ylabel("Voltage (V)")
+	plt.legend(["Laser", "Microphone"], loc = "best")
+plt.show()
+
+##### Aggregate Plots #####
 fig, ax = plt.subplots(1,1)
 L.aggrigate(collection_slice = slice(2, 11, 1))
 L.agg.plot(tmin=2.8e-4, tmax = 3e-4, ax = ax, c = "r")
 M.aggrigate(collection_slice = slice(2, 11, 1))
 M.agg.plot(tmin=2.8e-4, tmax = 3e-4, ax = ax, c = "b")
-
 plt.title("Aggregate Pulses of Laser Ablation \n as Recorded by Microphone and Laser Deflection")
-plt.xlabel("Time (s)")
+x_ticks = [0.0002800, 0.0002825, 0.0002850, 0.0002875, 0.0002900, 0.0002925, 0.0002950, 0.0002975, 0.0003000]
+x_labels = [280.0, 282.5, 285.0, 287.5, 290.0, 292.5, 295.0, 297.5, 300.0]
+plt.xticks(ticks = x_ticks, labels = x_labels)
+plt.xlabel("Time (us)")
 plt.ylabel("Voltage (V)")
 plt.legend(["Laser", "Microphone"], loc = "best")
-plt.show()
+
+
