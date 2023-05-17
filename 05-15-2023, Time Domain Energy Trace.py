ab48369abd9e7466ca5e1517031aacabcfae46f4
Ryan Schlimme <98181475+ryanschlimme@users.noreply.github.com>
2023-05-17 16:24:57 -0500

    Transfer Function
    Using new data of 100 pulses scanned over integer increments from 14 to 19 J. Estimating transfer function using Fourier analysis. Developed method for time domain averaging using NumPy array conversions of ray TimeSeries objects.

--- 05-15-2023, Time Domain Energy Trace.py
+++ 05-15-2023, Time Domain Energy Trace.py
@@ -1,15 +1,15 @@
 # 15 May 2023
 # Ryan Schlimme
 
-# Comparing time domain laser pulses to calibrated microphone reading at energies scaling from 14 to 19 kJ in 1 kJ increments. Applying known 0.00068 V/Pa offset to microphone to transfer to pressure signal. Still searching for laser transfer function.
+# Comparing time domain laser pulses to calibrated microphone reading at energies scaling from 14 to 19 J in 1 J increments. Applying known 0.00068 V/Pa offset to microphone to transfer to pressure signal. Still searching for laser transfer function.
 # Noticed we are matching low frequecies well but exhibiting high frequency oscillations. Quantum noise? Or actual detection?
 
-f_name_index = [r"C:\Users\Ryan Schlimme\OneDrive\Desktop\Research\Data\ene_scan\laser-X_microphone-Y0.00068V-per-Pa\iter_" + str(i) + ".tdms" for i in range(6)] # create a variable pointing to file (change Ryan Schlimme to ryans)
+f_name_index = [r"C:\Users\ryans\OneDrive\Desktop\Research\Data\ene_scan\laser-X_microphone-Y0.00068V-per-Pa\iter_" + str(i) + ".tdms" for i in range(6)] # create a variable pointing to file (change Ryan Schlimme to ryans)
 
 import sys
 import matplotlib.pyplot as plt
 
-sys.path.append(r"C:\Users\Ryan Schlimme\OneDrive\Desktop\Research\brownian\src") 	# append path to brownian src folder (change Ryan Schlimme to ryans)
+sys.path.append(r"C:\Users\ryans\OneDrive\Desktop\Research\brownian\src") 	# append path to brownian src folder (change Ryan Schlimme to ryans)
 from time_series import CollectionTDMS								# pull function from time_series module
 
 
@@ -26,18 +26,18 @@ for n in N:
 	L.apply("detrend", mode = "linear", inplace = True)
 	M.apply("calibrate", cal = 0.185, inplace = True)
 	M.apply("detrend", mode = "linear", inplace = True)
-	M.apply("shift", tau = 0.000025, inplace = True)
-	Npts = L.r / (2*750000)							# Bin_average to max frequency of 500 kHz
+	M.apply("shift", tau = 0.0000235, inplace = True)
+	Npts = L.r / (2*500000)							# Bin_average to max frequency of 500 kHz
 	L.apply("bin_average", Npts = Npts, inplace = True)
 	M.apply("bin_average", Npts = Npts, inplace = True)
 ##### Aggregate Plots #####
 	fig, ax = plt.subplots(1,1)
 	L.aggrigate(collection_slice = slice(2, 100, 1))
-	L.agg.plot(tmin=2.8e-4, tmax = 3e-4, ax = ax, c = "r", tunit = "us")
+	L.agg.plot(tmin=2.6e-4, tmax = 3e-4, ax = ax, c = "r", tunit = "us")
 	M.aggrigate(collection_slice = slice(2, 100, 1))
-	M.agg.plot(tmin=2.8e-4, tmax = 3e-4, ax = ax, c = "b", tunit = "us")
+	M.agg.plot(tmin=2.6e-4, tmax = 3e-4, ax = ax, c = "b", tunit = "us")
 	plt.suptitle("Aggregate Pulses of Laser Ablation \n as Recorded by Microphone and Laser Deflection", fontsize = 11)
-	string = "\n" + str(n + 14) + "kJ"
+	string = "\n" + str(n + 14) + " J"
 	plt.title(string, fontsize = 9)
 	plt.xlabel("Time (us)")
 	plt.ylabel("Voltage (V)")
---
+++ 05-16-2023, Energy Trace Transfer Func.py
@@ -0,0 +1,128 @@
+# 16 May 2023
+# Ryan Schlimme
+
+# Attempt to identify transfer function using the following methodology
+
+# Correct mic signal 0.00068 V/Pa
+# Aggregate in time domain using some subset of the data n < nshot????
+# Fourier transform microphone M(f) and laser L(f)
+# Ensemble average the two FFTs 
+# Estimate transfer function Hbar ~ Ensemble avg of (M(f)/L(f))
+# Fit transfer function H(f) = A(f)e^i(phi(f)) = Re(f) + iIm(f) (fit amplitude and phase separately)
+#		np.abs(Hbar) * e ** (i * np.arg(Hbar)), need to decide what np function to use for angle (arctan2 or angle maybe)
+# Using fit result H(f), correct laser voltage signal
+
+
+f_name_index = [r"C:\Users\Ryan Schlimme\OneDrive\Desktop\Research\Data\ene_scan_laserX_microphoneY\iter_" + str(i) + ".tdms" for i in range(6)] # create a variable pointing to file (change Ryan Schlimme to ryans)
+
+import sys
+import matplotlib.pyplot as plt
+from scipy.fft import rfft, rfftfreq, irfft
+from scipy.optimize import curve_fit
+import numpy as np
+
+sys.path.append(r"C:\Users\Ryan Schlimme\OneDrive\Desktop\Research\brownian\src") 	# append path to brownian src folder (change Ryan Schlimme to ryans)
+from time_series import CollectionTDMS								# pull function from time_series module
+from acoustic_entrainment import mic_response
+from brownian import logbin_func
+from sympy import divisors
+
+N = list(range(6))
+
+def A(x, e, f, g, h):
+	A = e * x ** 3 + f * x ** 2 + g * x + h
+	return A
+
+def Phi(f, a, b, c):
+	Phi = a * f ** 2 + b * f + c
+	return Phi
+
+fig, (ax0, ax1) = plt.subplots(1, 2)
+
+for n in N:
+	f_name = f_name_index[n]
+	L = CollectionTDMS(f_name)
+	L.set_collection("X")
+	M = CollectionTDMS(f_name)
+	M.set_collection("Y")
+##### Time Analysis to Compare Pulses ######
+	L.apply("detrend", mode = "linear", inplace = True)
+	M.apply("detrend", mode = "linear", inplace = True)
+	L.apply("time_gate", tmin = 2.8e-4, tmax = None, inplace = True)
+	M.apply("time_gate", tmin = 2.8e-4, tmax = None, inplace = True)
+	# M.apply("shift", tau = 0.0000235, inplace = True)
+	Npts = L.r / (2*200000)							# Bin_average to max frequency of 500 kHz
+	L.apply("bin_average", Npts = Npts, inplace = True)
+	M.apply("bin_average", Npts = Npts, inplace = True)
+	M.apply("correct", response = mic_response, recollect = True)
+	Mdata = np.array([shot.x for shot in M.collection]) # 2d array, each row is a different shot
+	Ldata = np.array([shot.x for shot in L.collection])
+	numcols = len(Mdata[0])
+	numrows = len(Mdata)
+	# print(divisors(numrows))
+
+	k = 5 	# number of rows in each average	
+
+# Initialize Dummy Paramters
+	kcopy = k
+	numavg = numrows / k
+	index = 0
+	Mavg = np.array([np.empty(numcols)])
+	Lavg = np.array([np.empty(numcols)])
+	k_new = k
+
+# Loop Through Array Taking Averages
+	while numavg > 0:
+		avgd_row = np.array([np.mean(Mdata[index:k_new, :], axis = 0)])
+		Mavg = np.append(Mavg, avgd_row, axis = 0)
+		index = index + k
+		k_new = k_new + k
+		numavg = numavg - 1
+# Remove Dummy 1st Row
+	Mavg = np.delete(Mavg, 0, axis = 0)
+
+# Initialize Dummy Paramters
+	k = kcopy
+	numavg = numrows / k
+	index = 0
+	Mavg = np.array([np.empty(numcols)])
+	Lavg = np.array([np.empty(numcols)])
+	k_new = k
+# Loop Through Array Taking Averages
+	while numavg > 0:
+		avgd_row = np.array([np.mean(Ldata[index:k_new, :], axis = 0)])
+		Lavg = np.append(Lavg, avgd_row, axis = 0)
+		index = index + k
+		k_new = k_new + k
+		numavg = numavg - 1
+# Remove Dummy 1st Row
+	Lavg = np.delete(Lavg, 0, axis = 0)
+
+##### FFT #####
+	avgMfft = 0
+	for n in Mavg:
+		avgMfft += rfft(n) / numcols
+	avgLfft = 0
+	for n in Lavg:
+		avgLfft += rfft(n) / numcols
+	Lfreq = rfftfreq(len(Ldata[0]), 1/L.r)
+##### Transfer Function #####
+	H = avgLfft / avgMfft
+# Log Bin Averaging Freq/Amplitude/Phase
+	LogFreq = logbin_func(Lfreq, Npts = 20) #10-50
+	LogAmp = logbin_func(np.abs(H), Npts = 20) #10-50
+	LogPhi = logbin_func(np.angle(H), Npts = 20)
+	# LogAmpUncertainty = logbin_func(np.abs(H), Npts = 20, func = np.std)
+	ax0.loglog(LogFreq[1:], LogAmp[1:])
+	popt, pcov = curve_fit(A, LogFreq[1:], LogAmp[1:])
+	# popt, pcov = curve_fit(A, LogFreq[1:], LogAmp[1:], sigma = LogAmpUncertainty[1:], absolute_sigma = True)
+	# print(popt)
+	# print(np.sqrt(np.diag(pcov)))
+	xvals = np.linspace(1e3, 2e5, 100000)
+	ax0.loglog(xvals, A(xvals, popt[0], popt[1], popt[2], popt[3]))
+	ax0.set_title("Amplitude Fit")
+	ax1.plot(LogFreq[1:], LogPhi[1:])
+	popt, pcov = curve_fit(Phi, LogFreq[1:], LogPhi[1:])
+	ax1.plot(xvals, Phi(xvals, popt[0], popt[1], popt[2]))
+	ax1.set_title("Phase Fit")
+plt.show()
