4ffc5f21148b284acd0b386eeda2d8745a42c3c8
Ryan Schlimme <98181475+ryanschlimme@users.noreply.github.com>
2023-06-12 09:16:53 -0500

    Refined Time Series and Frequency SNR

--- 06-05-2023, New System Time Domain.py
+++ 06-05-2023, New System Time Domain.py
@@ -13,77 +13,27 @@ sys.path.append(r"C:\Users\ryans\OneDrive\Desktop\Research\brownian\src") 	# app
 from time_series import CollectionTDMS
 from acoustic_entrainment import mic_response						
 
-
 N = list(range(8))
-fig, axes = plt.subplots(1, 8, sharey = True)
-fc_list = list(np.geomspace(10000, 2500000, 20))
+fig, axes = plt.subplots(8,1)
 
 for n, ax in zip(N, axes.flatten()):
-# Using low pass filtering OR bin averaging w/ mic correction
+# Using low pass filtering AND bin averaging w/ mic correction
 	f_name = f_name_index[n]
-# Initialize dummy arrays for SNR results as a function of fc
-	L_SNR_array = np.array([])
-	M_SNR_array = np.array([])
-	Lmax_V_array = np.array([])
-	Mmax_V_array = np.array([])
-	L_RMS_array = np.array([])
-	M_RMS_array = np.array([])
-	L_SNR_std_array = np.array([])
-	M_SNR_std_array = np.array([])
-	for fc in fc_list:
-		L = CollectionTDMS(f_name)
-		L.set_collection("X")
-		M = CollectionTDMS(f_name)
-		M.set_collection("Y")
-		L.apply("detrend", mode = "linear", inplace = True)
-		M.apply("detrend", mode = "linear", inplace = True)
-		L.apply("calibrate", cal = -1, inplace = True)
-		M.apply("shift", tau = 35e-6, inplace = True)
-		M.apply("lowpass", cutoff = fc, inplace = True)
-		L.apply("lowpass", cutoff = fc, inplace = True)
-		Npts = L.r/ (2 * fc)			# Neiquist Criterion given cutoff frequency
-		L.apply("bin_average", Npts = Npts, inplace = True)
-		M.apply("bin_average", Npts = Npts, inplace = True)
-		M.apply("correct", response = mic_response, recollect = True) 	# Calibration of microphone
-# Initialize dummy lists for Lmax_V, Mmax_V, L_RMS, M_RMS
-		Lmax_V = []
-		Mmax_V = []
-		L_RMS = []
-		M_RMS = []
-# Calculating RMS for each 
-		for shot_laser, shot_micro in zip(L.collection[2:], M.collection[2:]):
-			Lmax_V.append(max(shot_laser.time_gate(tmin = 360e-6, tmax = 400e-6)[1]))
-			L_RMS.append(np.sqrt(np.mean(shot_laser.time_gate(tmin = 200e-6, tmax = 300e-6)[1] ** 2)))
-			Mmax_V.append(max(shot_micro.time_gate(tmin = 360e-6, tmax = 400e-6)[1]))
-			M_RMS.append(np.sqrt(np.mean(shot_micro.time_gate(tmin = 200e-6, tmax = 300e-6)[1] ** 2)))
-# Calculating SNRmean and st dev = Lmax_V / L_RMS_array, standard deviation
-		L_SNR = np.mean(np.array(Lmax_V) / np.array(L_RMS))
-		M_SNR = np.mean(np.array(Mmax_V) / np.array(M_RMS))
-		L_SNR_array = np.append(L_SNR_array, L_SNR)
-		M_SNR_array = np.append(M_SNR_array, M_SNR)
-		Lmax_V_array = np.append(Lmax_V_array, np.mean(Lmax_V))
-		Mmax_V_array = np.append(Mmax_V_array, np.mean(Mmax_V))
-		L_RMS_array = np.append(L_RMS_array, np.mean(L_RMS))
-		M_RMS_array = np.append(M_RMS_array, np.mean(M_RMS))
-	fig, (ax0, ax1) = plt.subplots(1,2)
-	ax0.plot(fc_list, Lmax_V_array)
-	ax0.plot(fc_list, Mmax_V_array)
-	ax1.plot(fc_list, L_RMS_array)
-	ax1.plot(fc_list, M_RMS_array)
-	ax0.legend(["Lmax", "Mmax"], loc = "best")
-	ax1.legend(["Lrms", "Mrms"], loc = "best")
-	L_SNR_std_array = np.append(L_SNR_std_array, np.std(np.array(Lmax_V) / np.array(L_RMS)))
-	M_SNR_std_array = np.append(M_SNR_std_array, np.std(np.array(Mmax_V) / np.array(M_RMS)))
-	
-# Generate SNR(fc) plots
-	# np.linspace(10000, 
-	# np.geomspace(start, stop, npts) #Loglog scale 
-	ax.loglog(fc_list, L_SNR_array, color = "r")
-	ax.loglog(fc_list, M_SNR_array, color = "b")
-	plt.suptitle("Laser/Mic SNR Comparison")
-	string = "\n" + str(n + 12) + " J"
-	ax.set_title(string, fontsize = 9)
-	fig.supxlabel("Cutoff Frequency ($fc$)")
-	fig.supylabel("Avg of (Max Signal / RMS of Noise)")
-	plt.legend(["Laser SNR", "Mic SNR"], loc = "best")
+	L = CollectionTDMS(f_name)
+	L.set_collection("X")
+	M = CollectionTDMS(f_name)
+	M.set_collection("Y")
+	L.apply("detrend", mode = "linear", inplace = True)
+	M.apply("detrend", mode = "linear", inplace = True)
+	L.apply("calibrate", cal = -1, inplace = True)
+	M.apply("shift", tau = 35e-6, inplace = True)
+	M.apply("lowpass", cutoff = 2.5e6, inplace = True)
+	L.apply("lowpass", cutoff = 2.5e6, inplace = True)
+	Npts = L.r/ (2 * 2.5e6)			# Neiquist Criterion given cutoff frequency
+	L.apply("bin_average", Npts = Npts, inplace = True)
+	M.apply("bin_average", Npts = Npts, inplace = True)
+	L.aggrigate(collection_slice = slice(2, 500, 1))
+	L.agg.plot(tmin=3.6e-4, tmax = 4e-4, ax = ax, c = "r", tunit = "us")
+	M.aggrigate(collection_slice = slice(2, 500, 1))
+	M.agg.plot(tmin=3.6e-4, tmax = 4e-4, ax = ax, c = "b", tunit = "us")
 plt.show()
---
+++ 06-08-2023, New System FFT.py
@@ -0,0 +1,72 @@
+# 08 June 2023
+# Ryan Schlimme
+
+# Generating Fourier spectra and signal to noise ratios at energies scaling from 12 to 19 J in 1 J increments. Applying known calibration to microphone to transfer to pressure signal. Applying qualitative calibration to laser to match troughs.
+
+f_name_index = [r"C:\Users\ryans\OneDrive\Desktop\Research\Data\laserX_microphoneY_energy12-19_N8\iter_" + str(i) + ".tdms" for i in range(8)] # create a variable pointing to file (change Ryan Schlimme to ryans)
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.path.append(r"C:\Users\ryans\OneDrive\Desktop\Research\brownian\src") 	# append path to brownian src folder (change Ryan Schlimme to ryans)
+from time_series import CollectionTDMS
+from acoustic_entrainment import mic_response						
+
+N = list(range(8))
+
+for n in N:
+# Using low pass filtering OR bin averaging w/ mic correction
+	f_name = f_name_index[n]
+	L = CollectionTDMS(f_name)
+	L.set_collection("X")
+	M = CollectionTDMS(f_name)
+	M.set_collection("Y")
+	L.apply("detrend", mode = "linear", inplace = True)
+	M.apply("detrend", mode = "linear", inplace = True)
+	L.apply("calibrate", cal = -1/0.0002, inplace = True)
+	M.apply("shift", tau = 35e-6, inplace = True)
+	#M.apply("lowpass", cutoff = 2.5e6, inplace = True)
+	#L.apply("lowpass", cutoff = 2.5e6, inplace = True)
+	Npts = L.r/ (2 * 2.5e6)			# Neiquist Criterion given cutoff frequency
+	#L.apply("bin_average", Npts = Npts, inplace = True)
+	#M.apply("bin_average", Npts = Npts, inplace = True)
+	M.apply("correct", response = mic_response, recollect = True) 
+	# M.apply("calibrate", cal = 1/0.00068, inplace = True)
+	Ldata = np.array([shot.time_gate(tmin = 360e-6, tmax = 420e-6)[1] for shot in L.collection])
+	Mdata = np.array([shot.time_gate(tmin = 360e-6, tmax = 420e-6)[1] for shot in M.collection])
+	Ldata_noise = np.array([shot.time_gate(tmin = 300e-6, tmax = 360e-6)[1] for shot in L.collection])
+	Mdata_noise = np.array([shot.time_gate(tmin = 300e-6, tmax = 360e-6)[1] for shot in M.collection]) 
+	numcols = len(Ldata[0])
+	avgMfft = 0
+	for i in Mdata:
+		avgMfft += np.fft.rfft(i) / numcols
+	avgLfft = 0
+	for i in Ldata:
+		avgLfft += np.fft.rfft(i) / numcols
+	avgMfft_noise = 0
+	for i in Mdata_noise:
+		avgMfft_noise += np.fft.rfft(i) / numcols
+	avgLfft_noise = 0
+	for i in Ldata_noise:
+		avgLfft_noise += np.fft.rfft(i) / numcols
+	Lfreq = np.fft.rfftfreq(numcols, 1/L.r)
+	LSNR = np.abs(avgLfft) ** 2 / np.abs(avgLfft_noise) ** 2
+	MSNR = np.abs(avgMfft) ** 2 / np.abs(avgMfft_noise) ** 2
+	fig, (ax0, ax1) = plt.subplots(1,2, sharex = True, figsize = [8, 4])
+	string = "Flash Lamp Energy: " + str(n + 12) + " J"
+	plt.suptitle(string)
+	ax0.set_title("FFT Comparison", fontsize = 10.5)
+	ax1.set_title("SNR Comparison", fontsize = 10.5)
+	ax0.loglog(Lfreq[1:], np.abs(avgLfft[1:]) ** 2, color = "r")
+	ax0.loglog(Lfreq[1:], np.abs(avgMfft[1:]) ** 2, color = "b")
+	ax0.loglog(Lfreq[1:], np.abs(avgLfft_noise[1:]) ** 2, ls = "--", color = "r")
+	ax0.loglog(Lfreq[1:], np.abs(avgMfft_noise[1:]) ** 2, ls = "--", color = "b")
+	ax0.legend(["Laser Signal", "Mic Signal", "Laser Noise", "Mic Noise"], loc = "lower left")
+	ax1.loglog(Lfreq[1:], LSNR[1:], color = "r")
+	ax1.loglog(Lfreq[1:], MSNR[1:], color = "b")
+	ax1.legend(["Laser SNR", "Mic SNR"], loc = "upper right")
+	fig.supxlabel("Frequency (Hz)")
+	plt.xlim([Lfreq[1], Lfreq[-1]])
+	plt.subplots_adjust(left = 0.074, bottom = 0.11, right = 0.947, top = 0.872, wspace = 0.2, hspace = 0.2)
+	plt.savefig(r"C:\Users\ryans\OneDrive\Desktop\Research\Figures\No Bin Avg, No Low Pass\iter_" + str(n) + ".pdf", format = "pdf")
